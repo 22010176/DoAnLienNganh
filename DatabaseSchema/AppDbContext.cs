628ec10678a098bca969a65bc7b6dc2db845ce16
Do Duc Minh <22010176@st.phenikaa-uni.edu.vn>
2025-06-21 15:33:47 +0700

    ed

---
+++ DatabaseSchema/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using DatabaseSchema.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseSchema;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+  public DbSet<NguoiDung> NguoiDung { get; set; }
+}
